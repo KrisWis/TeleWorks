98e69a8c7013d9564c0cfade5ad5b7bc4ad1d4e1
Evgeniy <KworkEvgeniy@yandex.ru>
2024-06-23 12:04:56 +0300

    Улучшение оптимизации.

--- src/entities/Profile/ui/Profile__footer/Profile__footer.tsx
+++ src/entities/Profile/ui/Profile__footer/Profile__footer.tsx
@@ -6,19 +6,19 @@ export const Profile__footer: React.FC = (): React.JSX.Element => {
 
             <div className={styles.profile__footer__item}>
                 <img className={styles.profile__footer__item__img} src="icons/profile__footer/geo.png" alt="Иконка гео" />
-                <h4 className={styles.profile__footer__item__caption}>Гео / Язык</h4>
+                <h3 className={styles.profile__footer__item__caption}>Гео / Язык</h3>
                 <span className={styles.profile__footer__item__desc}>Россия / Русский</span>
             </div>
 
             <div className={styles.profile__footer__item}>
                 <img className={styles.profile__footer__item__img} src="icons/profile__footer/age.png" alt="Иконка гео" />
-                <h4 className={styles.profile__footer__item__caption}>Возраст</h4>
+                <h3 className={styles.profile__footer__item__caption}>Возраст</h3>
                 <span className={styles.profile__footer__item__desc}>1 год.</span>
             </div>
 
             <div className={styles.profile__footer__item}>
                 <img className={styles.profile__footer__item__img} src="icons/profile__footer/posts.png" alt="Иконка гео" />
-                <h4 className={styles.profile__footer__item__caption}>Постов</h4>
+                <h3 className={styles.profile__footer__item__caption}>Постов</h3>
                 <span className={styles.profile__footer__item__desc}>1 573</span>
             </div>
 
