b13802b09dfcf646215344617c6ad31a7d8e8202
Evgeniy <KworkEvgeniy@yandex.ru>
2024-06-23 11:59:32 +0300

    Улучшение оптимизации, добавление lazy loading.

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,5 +1,15 @@
-import { Profile, Stats } from "@/entities"
 import styles from './Main.module.scss';
+import { lazy } from "react";
+
+const Profile: React.LazyExoticComponent<React.FC<{}>> = lazy(() =>
+    import("@/entities")
+        .then(({ Profile }) => ({ default: Profile })),
+);
+
+const Stats: React.LazyExoticComponent<React.FC<{}>> = lazy(() =>
+    import("@/entities")
+        .then(({ Stats }) => ({ default: Stats })),
+);
 
 export const Main: React.FC = (): React.JSX.Element => {
     return (
