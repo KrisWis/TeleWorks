da49bd5220ed3b0a786925a54fc112bf18764dad
Evgeniy <KworkEvgeniy@yandex.ru>
2024-06-26 09:58:09 +0300

    Добавлен Eslint и StyleLint. Код изменён в соответствии с их требованиями.

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,21 +1,21 @@
 import { lazy } from 'react';
 
-export const Profile: React.LazyExoticComponent<React.FC<{}>> = lazy(() =>
+export const Profile: React.LazyExoticComponent<React.FC> = lazy(() =>
     import('./Profile/ui/Profile')
         .then(({ Profile }) => ({ default: Profile })),
 );
 
-export const Stats: React.LazyExoticComponent<React.FC<{}>> = lazy(() =>
+export const Stats: React.LazyExoticComponent<React.FC> = lazy(() =>
     import('./Stats/ui/Stats')
         .then(({ Stats }) => ({ default: Stats })),
 );
 
-export const Buy_ads: React.LazyExoticComponent<React.FC<{}>> = lazy(() =>
+export const Buy_ads: React.LazyExoticComponent<React.FC> = lazy(() =>
     import('./Buy_ads/ui/Buy_ads')
         .then(({ Buy_ads }) => ({ default: Buy_ads })),
 );
 
-export const Channel_stats: React.LazyExoticComponent<React.FC<{}>> = lazy(() =>
+export const Channel_stats: React.LazyExoticComponent<React.FC> = lazy(() =>
     import('./Channel_stats/ui/Channel_stats')
         .then(({ Channel_stats }) => ({ default: Channel_stats })),
 );
