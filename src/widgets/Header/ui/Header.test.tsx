67ae55d18615dbf3c05d6f1a12d643ea7b8526fc
Evgeniy <KworkEvgeniy@yandex.ru>
2024-07-09 23:06:44 +0300

    Add something with tests.

--- src/widgets/Header/ui/Header.test.tsx
+++ src/widgets/Header/ui/Header.test.tsx
@@ -1,21 +1,12 @@
 import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Header } from "./Header";
-import { userEvent } from "@storybook/test";
 import { renderWithRouter } from "@/shared/utils/renderWithRouter";
 
 describe("Header", () => {
   test("render test", () => {
     renderWithRouter(<Header />);
-    expect(screen.getByTestId("headerSwitcher")).toBeInTheDocument();
     expect(screen.getByTestId("headerLink")).toBeInTheDocument();
     expect(screen.getByTestId("headerDropDown")).toBeInTheDocument();
-  });
-
-  test("switchers click test", () => {
-    renderWithRouter(<Header />);
-    const switcher = screen.getByTestId("headerSwitcher");
-    userEvent.click(switcher);
-    expect(screen.getByTestId("headerSwitcher")).toHaveClass("Mui-checked");
   });
 });
