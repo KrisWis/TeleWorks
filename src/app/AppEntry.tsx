bc9f24b098b5720bd82908039001e22528942363
Evgeniy <KworkEvgeniy@yandex.ru>
2024-07-17 23:52:55 +0300

    Fix AppEntry.tsx

--- src/app/AppEntry.tsx
+++ src/app/AppEntry.tsx
@@ -1,10 +1,11 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./layouts/ui/BaseLayout";
 import { HeaderRoutes } from "@/shared/utils/HeaderRoutes";
 import { FooterRoutes } from "@/shared/utils/FooterRoutes";
 import { ReceiveNotificationRoutes } from "@/shared/utils/ReceiveNotificatons";
+import ReactDOM from "react-dom";
 
+// @ts-expect-error потому что тут ругается react-router-dom
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter basename="/TeleWorks/">
     <HeaderRoutes />
