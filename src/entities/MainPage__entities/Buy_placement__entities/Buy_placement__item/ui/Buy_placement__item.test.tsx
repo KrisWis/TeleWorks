ebcf7b9b61fdd41a9a8f4bcd1f946d073840107a
Evgeniy <KworkEvgeniy@yandex.ru>
2024-07-17 23:31:32 +0300

    Fix .yml file.

--- src/entities/MainPage__entities/Buy_placement__entities/Buy_placement__item/ui/Buy_placement__item.test.tsx
+++ src/entities/MainPage__entities/Buy_placement__entities/Buy_placement__item/ui/Buy_placement__item.test.tsx
@@ -5,12 +5,12 @@ import { Buy_placement__item } from "./Buy_placement__item";
 describe("BuyPlacement Item", () => {
   test("render test", () => {
     render(<Buy_placement__item amount={"amount"} text="text" />);
-    expect(screen.getByTestId("Buy_placement__item")).not.toBeInTheDocument();
-    expect(
-      screen.getByTestId("Buy_placement__item__amount")
-    ).not.toHaveTextContent("amount");
-    expect(
-      screen.getByTestId("Buy_placement__item__text")
-    ).not.toHaveTextContent("text");
+    expect(screen.getByTestId("Buy_placement__item")).toBeInTheDocument();
+    expect(screen.getByTestId("Buy_placement__item__amount")).toHaveTextContent(
+      "amount"
+    );
+    expect(screen.getByTestId("Buy_placement__item__text")).toHaveTextContent(
+      "text"
+    );
   });
 });
