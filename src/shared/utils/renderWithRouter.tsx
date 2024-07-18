ec7805911b821a769276ea398aa1fd13a21a7d24
KrisWis <94256853+KrisWis@users.noreply.github.com>
2024-07-18 23:23:32 +0300

    Update renderWithRouter.tsx

--- src/shared/utils/renderWithRouter.tsx
+++ src/shared/utils/renderWithRouter.tsx
@@ -1,14 +1,11 @@
+import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { App } from "@/app/layouts/ui/BaseLayout";
 
 export const renderWithRouter = (
   component: React.ReactElement,
   initialRoute: string = "/"
-): React.ReactElement => {
-  return (
-    <MemoryRouter initialEntries={[initialRoute]}>
-      <App />
-      {component}
-    </MemoryRouter>
+) => {
+  return render(
+    <MemoryRouter initialEntries={[initialRoute]}>{component}</MemoryRouter>
   );
 };
