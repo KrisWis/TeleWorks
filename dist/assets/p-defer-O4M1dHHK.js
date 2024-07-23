2fcd84d21529fdad7dfd82576f041a92207bb091
KrisWis <94256853+KrisWis@users.noreply.github.com>
2024-07-23 22:54:33 +0300

    Delete dist directory

--- dist/assets/p-defer-O4M1dHHK.js
+++
@@ -1,1 +0,0 @@
-var s=()=>{const e={};return e.promise=new Promise((r,o)=>{e.resolve=r,e.reject=o}),e};export{s as p};
--- dist/assets/use-isomorphic-layout-effect-CbMVGqk-.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./react-DAuq9I5S.js";var a=r.useLayoutEffect;export{a as i};
