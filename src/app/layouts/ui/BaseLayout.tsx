bc9b8aed1201cbfc82e1b21acec88a73333fab78
Evgeniy <KworkEvgeniy@yandex.ru>
2024-07-29 13:53:43 +0300

    Add loading for Pages.

--- src/app/layouts/ui/BaseLayout.tsx
+++ src/app/layouts/ui/BaseLayout.tsx
@@ -2,13 +2,17 @@ import "./BaseLayout.module.scss";
 import { Route, RouteProps, Routes } from "react-router-dom";
 import "@/shared/reset.scss";
 import { routes } from "@/app/routes/Routes";
+import { Suspense } from "react";
+import { SemipolarLoading } from "react-loadingg";
 
 export const App: React.FC = (): React.JSX.Element => {
   return (
-    <Routes>
-      {routes.map((route: RouteProps) => (
-        <Route key={route.path} path={route.path} element={route.element} />
-      ))}
-    </Routes>
+    <Suspense fallback={<SemipolarLoading />}>
+      <Routes>
+        {routes.map((route: RouteProps) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Suspense>
   );
 };
