8b9e3b6f6dec8873d38e7cdbb82013998749cae3
Evgeniy <KworkEvgeniy@yandex.ru>
2024-07-30 18:05:29 +0300

    Updates

---
+++ assets/index-CW9CyTgC.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["assets/Footer-C-8T8fxc.js","assets/react-BzMsQ5fY.js","assets/@babel-t2wm9ggw.js","assets/Footer-Mf4gAztV.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{_ as t}from"./index-B0Ar8FVj.js";import{r}from"./react-BzMsQ5fY.js";const _=r.lazy(()=>t(async()=>{const{Footer:o}=await import("./Footer-C-8T8fxc.js");return{Footer:o}},__vite__mapDeps([0,1,2,3])).then(({Footer:o})=>({default:o})));export{_ as F};
