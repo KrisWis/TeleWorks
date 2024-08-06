fa76da248a6ee161a55249c898ada0e333f33435
Evgeniy <KworkEvgeniy@yandex.ru>
2024-08-07 00:15:47 +0300

    fix select.

--- src/widgets/CreateOrderPage_widgets/CreateOrderCostContent/ui/CreateOrderCostPayment/model/CreateOrderCostPayment_data.ts
+++ src/widgets/CreateOrderPage_widgets/CreateOrderCostContent/ui/CreateOrderCostPayment/model/CreateOrderCostPayment_data.ts
@@ -1,8 +1,6 @@
 import { Select_Option } from "@/shared/ui-kit/Select/model/Select_types";
 
-export const CreateOrderCostPaymentBankCard__selectedOptions: Select_Option[] =
-  [
-    { value: "Банковская карта", label: "Банковская карта" },
-    { value: "Банковская карта2", label: "Банковская карта2" },
-    { value: "Банковская карта3", label: "Банковская карта3" },
-  ];
+export const CreateOrderCostPaymentBankCard__selectedOptions: Select_Option[] = [
+  { value: "Баланс TeleWorks", label: "Баланс TeleWorks" },
+  { value: "Банковская карта", label: "Банковская карта" },
+];
