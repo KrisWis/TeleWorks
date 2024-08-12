6ac637456dfd1d6fa8bfbeb450f521260974c25f
Evgeniy <KworkEvgeniy@yandex.ru>
2024-08-12 15:54:52 +0300

    Updates

---
+++ assets/LoadMore-DRUAbWBH.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-B8VV3c8w.js";import{a}from"./index-DVfhWGAV.js";const x=r.memo(({type:t,className:o})=>s.jsx(a,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
