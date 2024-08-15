28ed25d73a3163e0d524e60c2a1fe34fe7b1eebe
Evgeniy <KworkEvgeniy@yandex.ru>
2024-08-16 00:05:26 +0300

    Updates

---
+++ assets/LoadMore-C3Itrl0e.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-e8hmi6Ay.js";import{a}from"./index-W_dxwC2P.js";const x=r.memo(({type:t,className:o})=>s.jsx(a,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
