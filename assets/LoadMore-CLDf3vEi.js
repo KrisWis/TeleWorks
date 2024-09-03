a52dd9f1fd7a3a6aacd05e666728366455b7ae46
Evgeniy <KworkEvgeniy@yandex.ru>
2024-09-03 17:48:20 +0300

    Updates

---
+++ assets/LoadMore-CLDf3vEi.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-CfalvIRf.js";import{a}from"./index-BwmoaE5h.js";const x=r.memo(({type:t,className:o})=>s.jsx(a,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
