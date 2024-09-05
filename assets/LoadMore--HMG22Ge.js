fe927ca43cbafe2663236edc60bd2eb6a69f2610
Evgeniy <KworkEvgeniy@yandex.ru>
2024-09-05 23:32:58 +0300

    Updates

---
+++ assets/LoadMore--HMG22Ge.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-CfalvIRf.js";import{a}from"./index-DqpZN9zr.js";const x=r.memo(({type:t,className:o})=>s.jsx(a,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
