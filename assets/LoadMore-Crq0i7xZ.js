e711939d1c39076320641dfbd2e60baf21216a13
Evgeniy <KworkEvgeniy@yandex.ru>
2024-09-10 22:58:53 +0300

    Updates

---
+++ assets/LoadMore-Crq0i7xZ.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-DJlov51D.js";import{a}from"./index-DFmKn4p_.js";const x=r.memo(({type:t,className:o})=>s.jsx(a,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
