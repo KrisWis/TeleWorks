ee53beba4696dd8d73a6f3291be2125b5dff81a4
Evgeniy <KworkEvgeniy@yandex.ru>
2024-09-11 09:50:13 +0300

    Updates

---
+++ assets/LoadMore-DnPgTErO.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-BHR5W2Za.js";import{a}from"./index-DnnrQW3_.js";const x=r.memo(({type:t,className:o})=>s.jsx(a,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
