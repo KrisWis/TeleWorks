28c9f3b0a672b42cf8442a96ef26fa0657970e88
KrisWis <kworkEvgeniy@yandex.ru>
2024-10-03 18:27:53 +0300

    Updates

---
+++ assets/LoadMore-BQAserr8.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-CA4AiUJV.js";import{c as e}from"./index-D4bIpDeX.js";const x=r.memo(({type:t,className:o})=>s.jsx(e,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
