66b89c74b754a49fe8bf1720f91e3d1a9a2b2ad6
KrisWis <kworkEvgeniy@yandex.ru>
2024-10-09 23:35:10 +0300

    Updates

---
+++ assets/LoadMore-B0tc3LDW.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-GnO0LmOb.js";import{c as e}from"./index-CCNimua0.js";const x=r.memo(({type:t,className:o})=>s.jsx(e,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
