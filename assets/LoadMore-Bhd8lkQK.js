1d0a09bbb8db8670ecb539444a92f7f2b2619123
KrisWis <kworkEvgeniy@yandex.ru>
2024-10-26 20:21:51 +0300

    Updates

---
+++ assets/LoadMore-Bhd8lkQK.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-C3EGSwTV.js";import{B as e}from"./app-routes-BFRJzISi.js";const x=r.memo(({type:t,className:o})=>s.jsx(e,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
---
+++ assets/use-isomorphic-layout-effect-CaxrrJkV.js
@@ -0,0 +1,1 @@
+import{r}from"./react-C3EGSwTV.js";var a=r.useLayoutEffect;export{a as i};
