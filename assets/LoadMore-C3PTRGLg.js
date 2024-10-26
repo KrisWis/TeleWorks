b4381e29f69ce737c17ee82ae30a9bc3bf3b7280
KrisWis <kworkEvgeniy@yandex.ru>
2024-10-26 20:32:45 +0300

    Updates

---
+++ assets/LoadMore-C3PTRGLg.js
@@ -0,0 +1,1 @@
+import{r,j as s}from"./react-C3EGSwTV.js";import{B as e}from"./app-routes-B1pE1BO8.js";const x=r.memo(({type:t,className:o})=>s.jsx(e,{type:t,className:o,text:"Загрузить еще"}));export{x as L};
