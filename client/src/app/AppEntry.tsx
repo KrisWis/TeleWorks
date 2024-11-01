6f2667af3a7074ac7cdc4f279f2f44b48c526164
KrisWis <kworkEvgeniy@yandex.ru>
2024-11-01 15:16:03 +0300

    Update ReadME.

--- client/src/app/AppEntry.tsx
+++ client/src/app/AppEntry.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "@/shared/config/store/AppStore";
 import { ScrollToTop } from "@/shared/utils/ScrollToTop";
 
+// TODO: убрать из ридми текст о заморозке при разморозке проекта
 // TODO: проверить кроссбраузерность в конце разработки проекта
 // TODO: сделать e2e тесты для useLocalStorage, useIndexedDB, добавления конкретных изображений на странице редактирования профиля (проверка ошибок)
 
