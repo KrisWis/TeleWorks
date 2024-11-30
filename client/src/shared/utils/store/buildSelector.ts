3529caf551af9d5d4988da38780c0296c4a73839
KrisWis <kworkEvgeniy@yandex.ru>
2024-11-30 22:58:39 +0300

    Typisation of buildSelector.

--- client/src/shared/utils/store/buildSelector.ts
+++ client/src/shared/utils/store/buildSelector.ts
@@ -1,21 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { StoreSchema, useAppSelector } from "@/shared/config/store/AppStore";
 import { shallowEqual } from "react-redux";
 
-type Selector<T> = (state: StoreSchema) => T;
-type Result<T> = [() => T, Selector<T>];
+type Selector<T, Args extends any[]> = (state: StoreSchema, ...args: Args) => T;
+type Hook<T, Args extends any[]> = (...args: Args) => T;
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>];
 
-export function buildSelector<T>(
-  selector: Selector<T>,
-  customShallowEqual?: boolean,
-): Result<T> {
-  const useSelectorHook = () => {
-    if (customShallowEqual) {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      return useAppSelector(selector, shallowEqual);
-    } else {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      return useAppSelector(selector);
-    }
+export function buildSelector<T, Args extends any[] = []>(
+  selector: Selector<T, Args>,
+  isShallowEqual?: boolean,
+): Result<T, Args> {
+  const useSelectorHook: Hook<T, Args> = (...args: Args) => {
+    const ShallowEqual = isShallowEqual ? shallowEqual : undefined;
+
+    return useAppSelector((state) => selector(state, ...args), ShallowEqual);
   };
 
   return [useSelectorHook, selector];
